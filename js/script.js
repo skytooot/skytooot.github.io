5368bc9173896d7cec002f932e11e75aa6e0a116
skytooot <matheo.vintrou@gmail.com>
2025-10-12 22:58:29 +0200

    update theme button

--- js/script.js
+++ js/script.js
@@ -2,5 +2,5 @@ const themeButton = document.getElementById('themeButton');
 const html = document.documentElement;
 
 themeButton.addEventListener('click', () => {
-    html.classList.toggle('white-theme');
+    themeButton.textContent = html.classList.toggle('white-theme') ? '🌙' : '☀️';
 });
