cb7525cd273ae1e3ea19a87dd264ce323ed9b249
Mathéo Vintrou <matheo.vintrou@gmail.com>
2025-12-28 19:58:00 +0100

    vueJS

--- README.md
+++
@@ -1,3 +0,0 @@
-# skytooot.github.io
-
-Lien vers le site internet : https://skytooot.github.io/
--- js/script.js
+++
@@ -1,6 +0,0 @@
-const themeButton = document.getElementById('themeButton');
-const html = document.documentElement;
-
-themeButton.addEventListener('click', () => {
-    themeButton.textContent = html.classList.toggle('white-theme') ? '🌙' : '☀️';
-});
